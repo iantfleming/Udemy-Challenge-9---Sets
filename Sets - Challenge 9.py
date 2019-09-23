f5b847ed0f503856a5dcbde7f7e8eda3adcfa7bc
“Codeboy3000” <iantfleming1992@gmail.com>
2019-09-23 13:02:07 +0100

    Python file

--- Sets - Challenge 9.py
+++ Sets - Challenge 9.py
@@ -1,19 +1,3 @@
-# Create a program that takes some text and returns a list of
-# all the characters in the text that are not vowels, sorted in
-# alphabetical order.
-#
-# You can either enter the text from the keyboard or
-# initialise a string variable with the string.
-
-# text = input("Please enter some text: ")
-#
-# for char in text:
-#     if char in'AEIOUaeiou' or char in ' ':
-#         continue
-#     else:
-#         x = set(char)
-#         print(sorted(x),end='')
-
 sampleText = "Python is a very powerful language"
 vowels = frozenset("aeiou")
 finalSet = set(sampleText).difference(vowels)
